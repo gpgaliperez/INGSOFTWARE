ed02c4a2ae4734ea558f19df24fd80c413e5bcfb
gpgaliperez <gp.galiperez@gmail.com>
2020-09-29 10:06:29 -0300

    2.e Modificación en Release 1

--- app/src/main/java/com/example/ingsoftware/MainActivity.java
+++ app/src/main/java/com/example/ingsoftware/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Hola mundo");
+        System.out.println("Modificación en Release 1!");
 
     }
 }
