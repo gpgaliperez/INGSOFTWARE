73b31bd62c95f698733e61aa7fe7361b529b1d84
gpgaliperez <gp.galiperez@gmail.com>
2020-09-29 23:52:16 -0300

    2.j Modificación Feature B

--- app/src/main/java/com/example/ingsoftware/MainActivity.java
+++ app/src/main/java/com/example/ingsoftware/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("Hola mundo");
         System.out.println("Modificación 2.j - A");
+        System.out.println("Modificación 2.j - B");
     }
 }
