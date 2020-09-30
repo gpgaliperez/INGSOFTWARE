feac0de4e60f9127664dea8137da290aecf60e77
gpgaliperez <gp.galiperez@gmail.com>
2020-09-30 00:02:51 -0300

    Revert "2.j Modificación Feature B"
    This reverts commit 73b31bd62c95f698733e61aa7fe7361b529b1d84.

--- app/src/main/java/com/example/ingsoftware/MainActivity.java
+++ app/src/main/java/com/example/ingsoftware/MainActivity.java
@@ -12,6 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("Hola mundo");
         System.out.println("Modificación 2.j - A");
-        System.out.println("Modificación 2.j - B");
     }
 }
